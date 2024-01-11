1af5c8de0cd37459cecdb21319484b0982865be9
grgr589 <gogo589@naver.com>
2024-01-11 21:55:55 +0900

    feat : initialize project setting

---
+++ src/main/java/com/example/instargram_clone/InstargramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.instargram_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstargramCloneApplication.class, args);
+    }
+
+}
