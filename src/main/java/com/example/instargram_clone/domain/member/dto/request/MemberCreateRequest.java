7a4558a897b3e2342013babff10514835d6cdca6
grgr589 <gogo589@naver.com>
2024-01-31 23:59:13 +0900

    refactor : request null 처리

--- src/main/java/com/example/instargram_clone/domain/member/dto/request/MemberCreateRequest.java
+++ src/main/java/com/example/instargram_clone/domain/member/dto/request/MemberCreateRequest.java
@@ -1,15 +1,21 @@
 package com.example.instargram_clone.domain.member.dto.request;
 
 import com.example.instargram_clone.domain.member.entity.Member;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class MemberCreateRequest {
 
+    @NotNull(message = "이메일을 입력해주세요")
     private String email;
+    @NotNull(message = "이름을 입력해주세요")
     private String name;
+    @NotNull(message = "비밀번호를 입력해주세요")
     private String password;
+
     private String profileurl;
+
     private String statusMessage;
 
     public MemberCreateRequest(String mail, String testUser, String password) {
